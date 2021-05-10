24a3dc6cce68d2af332786cbb8e7cb020e2199cc
SeungsooChoi <34928445+SeungsooChoi@users.noreply.github.com>
2021-05-10 18:04:06 +0900

    feat : 네이버 지도api 사용 - dotenv 사용하여 키 숨김

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
-function App() {
-  return <div className="App">Swim-Map</div>;
+import { Component } from "react";
+import dotenv from "dotenv";
+dotenv.config();
+
+class Map extends Component {
+  componentDidMount() {
+    console.log(process.env.REACT_APP_NAVER_CLIENT_ID);
+
+    const map = new window.naver.maps.Map("map", {
+      center: new window.naver.maps.LatLng(37.3595704, 127.105399),
+      zoom: 10,
+    });
+  }
+
+  render() {
+    return <div id="map">Swim-Map</div>;
+  }
 }
 
-export default App;
+export default Map;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
+import Map from "./App";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Map />
   </React.StrictMode>,
   document.getElementById("root")
 );
